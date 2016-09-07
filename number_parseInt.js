c667bbf4240c0130cb2ff0db5a8b1d757059c4c5
yahoosaran <yahoosaran@gmail.com>
2016-09-07 23:15:36 +0530

    Initial commit

---
+++ number_parseInt.js
@@ -0,0 +1,14 @@
+var string = parseInt("this is test strig");
+
+var nowString = "now string";
+
+console.log( typeof( string ));
+
+console.log(isNaN(string));
+
+console.log(isNaN(nowString));
+
+var testNumber = 5;
+
+console.log( isNaN(testNumber));
+
